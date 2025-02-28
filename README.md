1e54d01573cc2350baaaf68bddfa9ddcb3db6619
Viktor Kolev <viktorkolev0707@gmail.com>
2025-02-28 12:05:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# VIALOGUE
+
+### Start chatting!
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+     <h1>VIALOGUE</h1>
+     <p>Start chatting!</p>
+    </div>
+  )
+}
+
+export default App
