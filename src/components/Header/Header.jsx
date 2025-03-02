cbaf8b4777c85718b4d02e403086bcdcbadc10d8
Viktor Kolev <118537287+smithy773@users.noreply.github.com>
2025-03-02 02:11:33 +0200

    Revert "Users"

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
+import './Header.css';
+
 export default function Header() {
   return (
-    <header className='flex items-center justify-between py-2 px-4 bg-light-blue'>
+    <header className='flex items-center justify-between py-2 px-4  bg-light-blue'>
       <span className='text-6xl'>LOGO</span>
       <div className='flex gap-4 mr-8'>
         <button className='border-4 border-dark-blue rounded-lg py-1 px-2 bg-light-blue hover:bg-blue hover:border-blue cursor-pointer transition'>Log In</button>
